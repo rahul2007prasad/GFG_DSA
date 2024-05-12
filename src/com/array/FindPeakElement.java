a51fa4fd807ab3d8282511b60c772c8a4774d2ad
rahul2007prasad <107prasad107@gmail.com>
2024-05-12 15:57:58 +0530

    peak element binary search

--- src/com/array/FindPeakElement.java
+++ src/com/array/FindPeakElement.java
@@ -6,6 +6,8 @@ public class FindPeakElement {
 	public static void main(String[] args) {
 		 int arr[] = { 1, 3, 20, 4, 1, 0 };
 		System.out.println("Peak element index: " + findPeak(arr));
+	
+		System.out.println("Peak element using binaty searh : " + peakelementBinarySearch(arr));
 	}
 	
 	public static int findPeak(int arr[]) {
@@ -28,5 +30,35 @@ public class FindPeakElement {
 		
 		
 		
+	}
+	
+	public static int peakelementBinarySearch(int arr[]) {
+		int n = arr.length;
+		
+		if(n== 1)
+			return arr[0];
+		
+		if(arr[0] > arr[1])
+			return arr[0];
+		
+		if(arr[n-1] > arr[n-2])
+			return arr[n-1];
+		
+		int low = 1;
+		int high = n-2;
+		while(low <= high) {
+			int mid = low + ((high -low) /2);
+			
+			if(arr[mid -1] < arr[mid] && arr[mid] > arr[mid+1])
+				return arr[mid];
+			
+			if(arr[mid] > arr[mid -1])
+				low = mid +1;
+			else
+				high = mid -1;
+		}
+		return 0;
+			
+				
 	}
 }
